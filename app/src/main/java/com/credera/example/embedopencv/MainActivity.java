3c8fb2ddae9c1fd92f9b5041d2c5cd8e665ae4f7
Samuel Khzym <smryk7@gmail.com>
2020-05-30 18:53:43 -0400

    OpenCV pipeline modified so that it would isolate the individual highlighted words and save them as an ArrayList of Bitmaps. App now lays out individual texts underneath initial interface (to be changed).

--- app/src/main/java/com/credera/example/embedopencv/MainActivity.java
+++ app/src/main/java/com/credera/example/embedopencv/MainActivity.java
@@ -1,6 +1,7 @@
 package com.credera.example.embedopencv;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
