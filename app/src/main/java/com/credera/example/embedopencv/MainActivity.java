9731dc37cc420a8f9eabafb9a33b411aa26becc6
Samuel Khzym <smryk7@gmail.com>
2020-05-29 22:25:33 -0400

    Image selector working. Resize image not implemented yet.

--- app/src/main/java/com/credera/example/embedopencv/MainActivity.java
+++ app/src/main/java/com/credera/example/embedopencv/MainActivity.java
@@ -2,11 +2,15 @@ package com.credera.example.embedopencv;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -14,6 +18,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import org.opencv.android.OpenCVLoader;
@@ -26,9 +31,14 @@ import java.lang.ref.WeakReference;
 
 import butterknife.internal.DebouncingOnClickListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int RESULT_LOAD_IMAGE = 1;
 
     private static final String TAG = "MainActivity";
+    private ImageView uploadedImage;
+    private int imageMaxHeight;
+    private Button uploadImageButton;
 
     static {
         if (!OpenCVLoader.initDebug()){
@@ -38,7 +48,40 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        uploadedImage = (ImageView) findViewById(R.id.imageToUpload);
+        imageMaxHeight = uploadedImage.getHeight();
+        uploadImageButton = (Button) findViewById(R.id.uploadImageButton);
+
+        uploadImageButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.uploadImageButton:
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            uploadedImage.setImageURI(selectedImage);
+
+            //FIGURE OUT HOW TO RESIZE IMAGE AND NOT EAT THE BUTTON HERE
+
+        }
+    }
+
+    /*private ImageView imageView;
     private Bitmap processedBitmap;
 
     @Override
@@ -206,6 +249,6 @@ public class MainActivity extends AppCompatActivity {
 
             return processedBitmap;
         }
-    }
+    }*/
 
 }
