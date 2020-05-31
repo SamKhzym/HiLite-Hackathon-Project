218d206957ad90beda3e6454ecde6b5b26807edb
Samuel Khzym <smryk7@gmail.com>
2020-05-30 20:53:24 -0400

    Text Recognizer working. Just outputs finished text to Logcat.

--- app/src/main/java/com/credera/example/embedopencv/MainActivity.java
+++ app/src/main/java/com/credera/example/embedopencv/MainActivity.java
@@ -28,6 +28,7 @@ import java.util.Random;
 import butterknife.internal.DebouncingOnClickListener;
 import com.google.android.gms.vision.text.Text;
 import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -37,8 +38,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ImageView uploadedImage;
     private int imageMaxHeight;
     private Button uploadImageButton, convertImageButton;
+    private TextRecognizer recognizer;
 
     private ArrayList<Bitmap> highlightedTexts = new ArrayList<Bitmap>();
+    private ArrayList<String> recognizedText = new ArrayList<String>();
 
     static {
         if (!OpenCVLoader.initDebug()){
@@ -61,6 +64,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         uploadImageButton.setOnClickListener(this);
         convertImageButton.setOnClickListener(this);
+
+        recognizer = new TextRecognizer.Builder(MainActivity.this).build();
     }
 
     @Override
@@ -73,6 +78,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             case R.id.convertPicture:
                 displayHighlightedTexts();
+                getTextFromBitmaps();
 
         }
     }
@@ -97,6 +103,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             ImageView newImg = new ImageView(this);
             newImg.setImageBitmap(highlightedTexts.get(i));
             layout.addView(newImg);
+        }
+
+    }
+
+    private void getTextFromBitmaps() {
+
+        for (int i = 0; i < highlightedTexts.size(); i++) {
+            String str = RecognizeText.extractTextFromImage(recognizer, highlightedTexts.get(i));
+            recognizedText.add(str);
+            Log.d("TESTING RECOGNITION", str);
         }
 
     }
