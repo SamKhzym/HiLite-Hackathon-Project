8860e4da6beb97aeeac890d08d34fd391838217b
Samuel Khzym <smryk7@gmail.com>
2020-05-31 03:25:43 -0400

    reeeeeeeeeeeeeeeeeeeeeeeeee

--- app/src/main/java/com/credera/example/embedopencv/ArrayToCsv.java
+++ app/src/main/java/com/credera/example/embedopencv/ArrayToCsv.java
@@ -1,27 +1,57 @@
 package com.credera.example.embedopencv;
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
 import java.io.*;
+import java.util.ArrayList;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class ArrayToCsv {
 
-    public static String toCSV(String[] array) throws IOException{
-        String result = "";
-        if (array.length > 0) {
+    private static final String FILE_NAME = "ExportedCSV.txt";
+    private static String masterString = "";
+
+    public static void exportCSV(ArrayList<String> strings) throws IOException {
+        String masterString = toCSV(strings);
+    }
+
+    public static String toCSV(ArrayList<String> array) throws IOException{
+        masterString = "";
+        if (array.size() > 0) {
             StringBuilder sb = new StringBuilder();
             for (String s : array) {
                 sb.append(s).append(",");
             }
-            result = sb.deleteCharAt(sb.length() - 1).toString();
+            masterString = sb.deleteCharAt(sb.length() - 1).toString();
         }
-        FileWriter out = null;
+        return masterString;
+    }
 
+    public void save(Context ctx) {
+
+        FileOutputStream fos = null;
         try {
-            out = new FileWriter("output.txt");
-            out.write(result);
-            } finally {
-            if (out != null) {
-                out.close();
+            fos = ctx.openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fos.write(masterString.getBytes());
+
+            Log.d("TEST", ("Saved to " + ctx.getFilesDir() + "/" + FILE_NAME));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
-        return "";
+
     }
+
 }
